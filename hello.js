9616d45c7aae513ef24b9802beaf770ca8e8a0c7
eyasfitanjilaantu <eyasfitanjila2002@gmail.com>
2024-06-27 17:12:12 +0600

    number variable

--- hello.js
+++ hello.js
@@ -1,8 +1,11 @@
-var myVillage ="gopinathput"
-console.log(myVillage);
+var myAge ="24"
+console.log(myAge);
 
 var myFatherName =`emdadul haue`
-console.log(myFatherName);
+console.log( typeof myFatherName);
 
 var myMotherName ='salma sultana'
 console.log( typeof myMotherName);
+
+var myNumber = 01304580549;
+console.log(typeof myNumber)
